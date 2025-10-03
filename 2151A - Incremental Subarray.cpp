b7fd330be0571a0d082ddb86f44f2e709cb206d5
FREDDY YU <FREDDYYU@COLLEGE.HARVARD.EDU>
2025-10-02 20:11:21 -0400

    untested coded solution

--- 2151A - Incremental Subarray.cpp
+++ 2151A - Incremental Subarray.cpp
@@ -3,20 +3,31 @@
 int main() {
     int numTestCases; std::cin >> numTestCases;
     for (int i = 0; i < numTestCases; i++) {
-
+        int finalAnswer;
+        bool finalAnswerIsOne = false;
 
         int maxWrittenNumber; std::cin >> maxWrittenNumber;
-        int arrayLength; std::cin >> arrayLength;
-        std::vector<int> array;
-
-        int nextElement;
+        int numDesiredNumbers; std::cin >> numDesiredNumbers;
         int maxDesiredNumber = 0;
-        for (int j = 0; j < arrayLength; j++) {
-            std::cin >> nextElement;
-            array.push_back(nextElement);
 
-            if ()
+
+        bool firstDesiredNumberIsOne = false;
+        int desiredNumber; std::cin >> desiredNumber;
+        if (desiredNumber == 1) {
+            firstDesiredNumberIsOne = true;
         }
+
+        for (int j = 1; j < numDesiredNumbers; j++) {
+            std::cin >> desiredNumber;
+            if (!firstDesiredNumberIsOne && desiredNumber == 1) {
+                finalAnswerIsOne = true;
+                break;
+            }
+            if (desiredNumber > maxDesiredNumber) maxDesiredNumber = desiredNumber;
+        }
+
+        finalAnswer = (finalAnswerIsOne) ? 1 : maxWrittenNumber - maxDesiredNumber + 1;
+        std::cout << finalAnswer << std::endl;
 
     }
     return 0;
